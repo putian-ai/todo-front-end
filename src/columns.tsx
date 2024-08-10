d06b9a34be801d86f5c4efe44f4688736b457540
Harrrryz <harry.wadu233@gmail.com>
2024-08-11 01:26:18 +0800

    change TodoTableData type and add className to column

--- src/columns.tsx
+++ src/columns.tsx
@@ -5,14 +5,18 @@ import { ColumnDef } from "@tanstack/react-table"
 // This type is used to define the shape of our data.
 export type TodoTableData = {
     id: number
-    item: string
-    planTime: string
+    item: string | null
+    planTime: string | null
 }
 
 export const columns: ColumnDef<TodoTableData>[] = [
     {
         accessorKey: "item",
         header: "Item",
+        cell: ({ row }) => {
+            const item: string = row.getValue("item")
+            return <div className="text-left font-medium">{item}</div>
+        },
     },
     {
         accessorKey: "plantime",
