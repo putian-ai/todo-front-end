52d26149fb953b5500f372ecf9e1bec232e1c0e6
Harrrryz <harry.wadu233@gmail.com>
2024-08-19 23:28:03 +0800

    add login protected and public router

---
+++ src/views/login.tsx
@@ -0,0 +1,51 @@
+import { UserAuthForm } from "@/components/auth/user-auth-form";
+
+function LoginPage() {
+  return (<>
+    <div className="container relative h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <div className="absolute inset-0 bg-zinc-900" />
+        <div className="relative z-20 flex items-center text-lg font-medium">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="mr-2 h-6 w-6"
+          >
+            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+          </svg>
+          Putian AI
+        </div>
+        <div className="relative z-20 mt-auto">
+          <blockquote className="space-y-2">
+            <p className="text-lg">
+              &ldquo;This library has saved me countless hours of work and
+              helped me deliver stunning designs to my clients faster than
+              ever before.&rdquo;
+            </p>
+            <footer className="text-sm">Sofia Davis</footer>
+          </blockquote>
+        </div>
+      </div>
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Login please
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email and password
+            </p>
+          </div>
+          <UserAuthForm />
+        </div>
+      </div>
+    </div>
+  </>);
+}
+
+export default LoginPage;
---
+++ src/views/protected.tsx
@@ -0,0 +1,11 @@
+import { ResizablePanel } from "@/components/ui/resizable";
+
+function ProtectedPage() {
+  return (<>
+    <ResizablePanel>
+      <div>Protected</div>
+    </ResizablePanel>
+  </>);
+}
+
+export default ProtectedPage;
