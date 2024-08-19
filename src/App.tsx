7eaa6f665796ca44473eab6e2536a0a4f9cb012d
Harrrryz <harry.wadu233@gmail.com>
2024-08-19 22:20:54 +0800

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
 import { Toaster } from './components/ui/toaster';
-import TodoPage from './TodoPage'
+import { Layout } from './layout';
+import PublicPage from './views/public';
+import LoginPage from './views/login';
+import ProtectedPage from './views/protected';
+import TodoPage from './TodoPage';
+
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    loader() {
+      // Our root route always provides the user, if logged in
+      return { user: 'fake user' };
+    },
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: TodoPage,
+      },
+
+      {
+        path: "protected",
+        // loader: protectedLoader,
+        Component: ProtectedPage,
+      },
+    ],
+  },
+  {
+    id: "login",
+    path: "login",
+    Component: LoginPage,
+  }
+]);
 
 
 function App() {
@@ -8,7 +42,7 @@ function App() {
   return (
     <>
       <div className="h-screen w-screen">
-        <TodoPage></TodoPage>
+        <RouterProvider router={router} fallbackElement={<p>Initial Load...</p>} />
       </div>
       <Toaster></Toaster>
     </>
